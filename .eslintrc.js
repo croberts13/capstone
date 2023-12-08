beb2f56881997158cc49a7f848c1e5729427d45d
croberts13 <croberts16@live.maryville.edu>
2023-12-08 17:58:42 -0500

    some changes and docs

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,7 +2,7 @@
 module.exports = {
     root: true,
     extends: ['eslint-config-mfe/eslintrc.es6.js'],
-    ignorePatterns: ['node_modules','client/'],
+    ignorePatterns: ['node_modules', 'client/'],
     rules: {
         camelcase: 'off',
         'arrow-parren': 'asNeeded',
