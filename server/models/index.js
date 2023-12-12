09f72a7bfc0a2a167e5c9e2b7ea9b1a8b7b7b653
croberts13 <croberts16@live.maryville.edu>
2023-12-12 16:42:59 -0500

    Update sslconf

--- server/models/index.js
+++ server/models/index.js
@@ -23,7 +23,7 @@ const sslConf =
           }
         : {};
 
-console.log({ sslConf });
+console.log(JSON.stringify(sslConf, null, 2));
 let sequelize;
 if (config.use_env_variable) {
     sequelize = new Sequelize(process.env[config.use_env_variable], {
